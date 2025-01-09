cdc9c65bf152be435382c29fe0d87c59565e3361
Egle Vondrak <egle.vondrak@gmail.com>
2025-01-09 11:28:08 +0100

    Initial commit

---
+++ src/components/Logo.jsx
@@ -0,0 +1,8 @@
+export default function Logo() {
+    return (
+        <div className='logo'>
+            <h1>Get Packed</h1>
+            <img src='/travel.svg' alt='Globe and Plane' />
+        </div>
+    );
+}
